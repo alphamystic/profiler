445fee2935038c07ea7ff6ba9ce0ba71d0c2ff1d
sam <sam@sam.sam>
2024-01-19 10:27:20 +0300

    working on templating

--- moja/ui/handlers/blank.go
+++ moja/ui/handlers/blank.go
@@ -12,9 +12,14 @@ func (hnd *Handler) Blank(res http.ResponseWriter, req *http.Request){
 }
 
 func (hnd *Handler) Test(res http.ResponseWriter, req *http.Request) {
-  http.Error(res, "An error occurred", http.StatusInternalServerError)
-	/*fmt.Println("HND WORKS")
-	res.Write([]byte("Handler Test Function Works"))*/
+  //http.Error(res, "An error occurred", http.StatusInternalServerError)
+  data := map[string]interface{}{
+    "header":"header.tmpl",
+    "sidebar":"sidebar.tmpl",
+    "body":"body.tmpl",
+    "footer":"footer.tmpl",
+  }
+	hnd.Tpl.ExecuteTemplate(res,"base.tmpl",data)
 }
 
 /*
@@ -24,4 +29,29 @@ So I want to serve a bunch of templates by combining them using text templates:
   Sidebar
   Body
   Footer
+
+
+given I have 3 files
+file 1
+This is File 1
+{{.DataForFile1}}
+
+file 2
+this is file 2
+{{.DataForFIle2}}
+
+file 3
+This is file 3
+{{.DataForFile3}}
+
+how can I add data rather combine them to the base Html File
+<html>
+<head>
+</head>
+<body>
+file 1
+file 2
+file 3
+</body>
+</html>
 */
